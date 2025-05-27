15e14dd397711b67d3cf39f544cecfebb8008beb
yongchane <202021113@sangmyung.kr>
2025-05-27 17:00:44 +0900

    회원가입 UI 작업중

--- src/page/login/register.tsx
+++ src/page/login/register.tsx
@@ -1,7 +1,44 @@
+import styled from "@emotion/styled";
 import React from "react";
 
-const register = () => {
-  return <div>register</div>;
+const Register = () => {
+  return (
+    <div className="bg-[#F7FBFE] w-full h-screen flex flex-col items-center ">
+      <div className="w-[320px] text-[35px] font-bold pt-[104px]">간편하게</div>
+      <div className="flex flex-col items-center justify-center pt-[70px]">
+        <div className="bg-[#F7FBFE] w-full h-screen flex flex-col items-center gap-[30px]">
+          <div>
+            <Font>별명</Font>
+            <InputBox placeholder="별명" />
+          </div>
+          <div>
+            <Font>나이</Font>
+            <InputBox placeholder="나이" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default register;
+export default Register;
+
+const InputBox = styled.input`
+  width: 320px;
+  height: 53px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  margin: 5px;
+  border: 1px solid #b3dbed;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+  color: gray;
+`;
+
+const Font = styled.div`
+  font-size: 22px;
+  font-weight: 600;
+  margin-left: 5px;
+  margin-bottom: 10px;
+`;
