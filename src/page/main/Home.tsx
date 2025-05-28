737ba123a3f64b185e8a9e7046f3b8ce42825b37
yongchane <202021113@sangmyung.kr>
2025-05-28 21:57:49 +0900

    주변, 길찾기 컴포넌트 작업

--- src/page/main/Home.tsx
+++ src/page/main/Home.tsx
@@ -1,17 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "../../components/Title";
 import InputPlace from "../../components/InputPlace";
+import PlaceIcon from "../../assets/Place.svg?react";
+import AtPlaceIcon from "../../assets/AtPlace.svg?react";
+import SearchIcon from "../../assets/SearchHome.svg?react";
+import AtSearchIcon from "../../assets/AtSearchHome.svg?react";
+import styled from "@emotion/styled";
+
+const selectBoxOptions = [
+  {
+    title: "주변",
+    icon: <PlaceIcon />,
+    hoverIcon: <AtPlaceIcon />,
+  },
+  {
+    title: "길찾기",
+    icon: <SearchIcon />,
+    hoverIcon: <AtSearchIcon />,
+  },
+];
 
 const Home = () => {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
   return (
     <div>
       {/* 로그인에 따른 닉네임 및 로직 구현 필요 */}
       <div className="flex flex-col items-center justify-center">
         <Title title="000님, 안녕하세요!" />
         <InputPlace />
+        <div className="flex gap-[26px] mt-[69px]">
+          {selectBoxOptions.map((option, index) => (
+            <SelectBox
+              key={index}
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
+            >
+              {hoveredIndex === index ? option.hoverIcon : option.icon}
+              <div>{option.title}</div>
+            </SelectBox>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
 export default Home;
+
+const SelectBox = styled.div`
+  width: 150px;
+  height: 150px;
+  background-color: #ffffff;
+  border-radius: 12px;
+  border: 1px solid #b3dbed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  font-weight: 700;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #6488ff;
+    color: #ffffff;
+  }
+`;
